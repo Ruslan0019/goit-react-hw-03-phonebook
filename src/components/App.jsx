10c149549f94ade22c0761ab624887719ed7b96a
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2023-12-15 16:58:26 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import styled from 'styled-components';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import ContactForm from './ContactForm/ContactForm';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+    filter: '',
+  };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { name, number, contacts } = this.state;
+
+    if (contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+    }));
+  };
+  render() {
+    const { contacts, name, number, filter } = this.state;
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return (
+      <Div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          name={name}
+          number={number}
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit}
+        />
+
+        <h2>Contacts</h2>
+        <Filter filter={filter} onChange={this.handleChange} />
+        <ContactList contacts={filteredContacts} />
+      </Div>
+    );
+  }
+}
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export default App;
